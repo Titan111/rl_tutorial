c176cbe082abcdbed19bf2f998aec40d4fa6329e
Titan111 <tomo_develop_singularity_111@ezweb.ne.jp>
2017-09-04 01:53:31 +0900

    make MultiData

--- bandit10/Data.py
+++ bandit10/Data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Data:
 	def __init__(self):
 		self.count_hit=0
@@ -6,10 +8,27 @@ class Data:
 		self.a_graph=[]
 		self.count = 1
 
-	def append(self,a,r):
+	def add(self,a,r):
 		if a == True:
 			self.count_hit+=1
 		self.sum_r +=  r
 		self.r_graph.append(self.sum_r/self.count)
 		self.a_graph.append(self.count_hit/self.count)
 		self.count += 1
+
+class MultiData:
+	def __init__(self,data_n):
+		self.data_n=data_n
+		self.r_graph=np.zeros(data_n)
+		self.a_graph=np.zeros(data_n)
+		self.sample_n=0
+
+	def add(self,data):
+		self.r_graph += np.array(data.r_graph)
+		self.a_graph += np.array(data.a_graph)
+		self.sample_n+= 1
+
+	def update(self):
+		print(self.sample_n)
+		self.r_graph /= self.sample_n
+		self.a_graph /= self.sample_n
